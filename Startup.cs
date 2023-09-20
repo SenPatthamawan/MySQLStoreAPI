f45e86cfb8b682cb72a156559de2091e4109d843
senpatthamawan <sen.patthamawan@gmail.com>
2023-09-20 14:06:38 +0700

    Fix error

--- Startup.cs
+++ Startup.cs
@@ -30,140 +30,79 @@ namespace MySQLStoreAPI
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
-                //c.SwaggerDoc("v1", new OpenApiInfo { Title = "MySQLStoreAPI", Version = "v1" });
-                c.SwaggerDoc("v1", new OpenApiInfo
-                {
-                    Title = "MySQL Store API",
-                    Version = "v1",
-                    Description = "An API to perform Store operations",
-                    TermsOfService = new Uri("https://example.com/terms"),
-                    Contact = new OpenApiContact
-                    {
-                        Name = "Patthamawan Chanthep",
-                        Email = "sen.patthamawan@gmail.com",
-                        Url = new Uri("https://github.com/SenPatthamawan"),
-                    },
-                    License = new OpenApiLicense
-                    {
-                        Name = "Store API LICX",
-                        Url = new Uri("https://example.com/license"),
-                    }
-                });
-
-                //เรียกใช้ Authentication------------------------------------
-                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
-                {
-                    Name = "Authorization",
-                    Type = SecuritySchemeType.ApiKey,
-                    Scheme = "Bearer",
-                    BearerFormat = "JWT",
-                    In = ParameterLocation.Header,
-                    Description = "JWT Authorization header using the Bearer scheme." + "\r\n\r\n"+
-                    "Enter 'Bearer' [space] and then your token in the text input below."+
-                    "\r\n\r\n Example: \"Bearer 12345abcdef\"",
-                });
-
-                c.AddSecurityRequirement(new OpenApiSecurityRequirement
-                {
-                    {
-                        new OpenApiSecurityScheme
-                        {
-                            Reference = new OpenApiReference
-                            {
-                                Type = ReferenceType.SecurityScheme,
-                                Id = "Bearer"
-                            }
-                        },
-                        new string[]{}
-                    }
-
-                });
-            
-            
-                //-----------------------------------------------------------------------------------
-
-                // เรียกใช้ ApplicationDbContext -------------------------------------------------------
-                //AddDbContextPool จะ connect database ให้ตลอด ไม่ต้องมา connect ใหม่เหมือน AddDbContext
-                services.AddDbContextPool<ApplicationDbContext>(options =>
-                    options.UseMySql(Configuration.GetConnectionString("DefaultConnection"),
-                    ServerVersion.AutoDetect(Configuration.GetConnectionString("DefaultConnection"))
-                ));
-                // ----------------------------------------------------------------------------------
-
-                // สำหรับเรียกใช้ Authentication --------------------------------------------------------
-                // For Identity  
-                services.AddIdentity<ApplicationUser, IdentityRole>()
-                    .AddEntityFrameworkStores<ApplicationDbContext>()
-                    .AddDefaultTokenProviders();
-
-                // Adding Authentication
-                services.AddAuthentication(options =>
-                {
-                    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-                    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-                    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
-                })
-
-                // Adding Jwt Bearer
-                .AddJwtBearer(options =>
-                {
-                    options.SaveToken = true;
-                    options.RequireHttpsMetadata = false;
-                    options.TokenValidationParameters = new TokenValidationParameters()
-                    {
-                        ValidateIssuer = true,
-                        ValidateAudience = true,
-                        ValidAudience = Configuration["JWT:ValidAudience"],
-                        ValidIssuer = Configuration["JWT:ValidIssuer"],
-                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["JWT:Secret"]))
-                    };
-                });
-
-                // ----------------------------------------------------------------------------------
-
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "MySQLStoreAPI", Version = "v1" });
             });
+
+            // เรียกใช้ ApplicationDbContext -------------------------------------------------------
+            //AddDbContextPool จะ connect database ให้ตลอด ไม่ต้องมา connect ใหม่เหมือน AddDbContext
+            services.AddDbContextPool<ApplicationDbContext>(options =>
+                options.UseMySql(Configuration.GetConnectionString("DefaultConnection"),
+                ServerVersion.AutoDetect(Configuration.GetConnectionString("DefaultConnection"))
+            ));
+            // ----------------------------------------------------------------------------------
+
+            // สำหรับเรียกใช้ Authentication --------------------------------------------------------
+            // For Identity  
+            services.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
+
+            // Adding Authentication
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+
+            // Adding Jwt Bearer
+            .AddJwtBearer(options =>
+            {
+                options.SaveToken = true;
+                options.RequireHttpsMetadata = false;
+                options.TokenValidationParameters = new TokenValidationParameters()
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidAudience = Configuration["JWT:ValidAudience"],
+                    ValidIssuer = Configuration["JWT:ValidIssuer"],
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["JWT:Secret"]))
+                };
+            });
+
+            // ----------------------------------------------------------------------------------
+
         }
-    
-        
+
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            //Swagger จะวิ่งไปหา Folder wwwroot ต้องเรียก UseStaticFiles ก่อนเรียก SwaggerUI
+            app.UseStaticFiles();
+
+            //ทำแค่ใน Dev mode ถ้าอยากขึ้น Production ด้วยให้เอา if ออกเลย
+            if (env.IsDevelopment())
             {
-                //ทำแค่ใน Dev mode ถ้าอยากขึ้น Production ด้วยให้เอา if ออกเลย
-                if (env.IsDevelopment())
-                {
-                    app.UseDeveloperExceptionPage();
-
-                    app.UseStaticFiles();
-                    //ให้ Swagger อ้างอิง Path มาที่ docs ของเรา มันจะมาหาเอง
-                    app.UseSwagger(c => {
-                        c.RouteTemplate ="docs/{documentName}/docs.json";
-                    });
-
-                    //Custom UI
-                    app.UseSwaggerUI(c => {
-                        c.SwaggerEndpoint("/docs/v1/docs.json", "MySQLStoreAPI v1");
-                        c.RoutePrefix = "docs";
-                        c.DocumentTitle = "MySQL Store API";
-                        c.InjectStylesheet("/docs-ui/custom.css");
-                        c.InjectJavascript("/docs-ui/custom.js");
-                    });
-                }
-
-                app.UseHttpsRedirection();
-
-                app.UseRouting();
-
-                // สำหรับเรียกใช้ Authentication --------------------------------------------------------
-                app.UseAuthentication();
-                // ----------------------------------------------------------------------------------
-
-                app.UseAuthorization();
-
-                app.UseEndpoints(endpoints =>
-                {
-                    endpoints.MapControllers();
-                });
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "MySQLStoreAPI v1"));
             }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            // สำหรับเรียกใช้ Authentication --------------------------------------------------------
+            app.UseAuthentication();
+            // ----------------------------------------------------------------------------------
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
     }
 }
 
